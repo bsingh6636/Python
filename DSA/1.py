3cc61833e8d42db342fc0568782d88d84015872a
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-04-02 00:30:28 +0530

    new

--- DSA/1.py
+++ DSA/1.py
@@ -224,3 +224,19 @@
 #     pass
 
 # numberCrown(3)
+
+# def nNumberTriangle(n: int) -> None:
+#     # Write your solution here.
+#     count = 1
+#     for i in range (1 , n + 1 ):
+#         for j in range ( 1 ,  i + 1 ):
+#             print(count , end = ' ')
+#             count += 1
+#         print()
+#     pass
+
+# nNumberTriangle(4)
+# 1 
+# 2 3 
+# 4 5 6 
+# 7 8 9 10 
