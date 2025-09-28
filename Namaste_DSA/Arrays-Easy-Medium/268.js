aa3cd11ed248e3dd259f96b5c3991dfa0c266f17
Brijesh Kumar Kushwaha <bsingh6636@gmail.com>
2025-09-29 01:42:04 +0530

    286

--- Namaste_DSA/Arrays-Easy-Medium/268.js
+++ Namaste_DSA/Arrays-Easy-Medium/268.js
@@ -42,7 +42,13 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-    
+    const n = nums.length;
+    const toalSum = n * ( n + 1) /2 
+    let partialSum = 0;
+    for( let i = 0 ; i < nums.length; i++ ){
+        partialSum += nums[i]
+    }
+    return toalSum - partialSum
 };
 
 
